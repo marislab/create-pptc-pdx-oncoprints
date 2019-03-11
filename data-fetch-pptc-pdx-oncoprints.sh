7f5fac85a24ad2db38cdb88fe8688a5a200881f9
Khushbu Patel <kpatel427@gatech.edu>
2019-03-11 13:30:03 -0400

    update

--- data-fetch-pptc-pdx-oncoprints.sh
+++ data-fetch-pptc-pdx-oncoprints.sh
@@ -3,6 +3,10 @@
 # INPUT files
 # Approved hugo symbols file = 2019-02-14-Hugo-Symbols-approved.txt
 # clinical file = 2019-02-09-pdx-clinical-final-for-paper.txt
+# source scripts = 
+# 1. mutation-color-function.R 
+# 2. demog-color-function.R 
+
 # MAF file = 2019-02-14-allpdx-clean-maf-240.rda
 # Expression matrix = 2019-02-14-PPTC_FPKM_matrix_withModelID-244.rda
 # focal CN matrix = 2019-02-27-short_cn_matrix_fpkm1.txt
@@ -19,40 +23,40 @@ chmod 755 pptc-pdx-oncoprints/data/ pptc-pdx-oncoprints/packages/
 cd ~/pptc-pdx-oncoprints/data/
 
 # 1. Approved Hugo Symbols file
-curl -o '2019-02-14-Hugo-Symbols-approved.txt' https://ndownloader.figshare.com/files/14460317
+wget --output-document='2019-02-14-Hugo-Symbols-approved.txt' https://ndownloader.figshare.com/files/14460317
 
 # 2. Clinical file
-curl -o 'pptc-pdx-clinical-web.txt' https://ndownloader.figshare.com/files/14508536
+wget --output-document='pptc-pdx-clinical-web.txt' https://ndownloader.figshare.com/files/14508536
 
 # 3. Download DNA MAF file
-curl -o '2019-02-14-allpdx-clean-maf-240.rda' https://ndownloader.figshare.com/files/14414198
+wget --output-document='2019-02-14-allpdx-clean-maf-240.rda' https://ndownloader.figshare.com/files/14414198
 
 # 4. Download Expression matrix
-curl -o '2019-02-14-PPTC_FPKM_matrix_withModelID-244.rda' https://ndownloader.figshare.com/articles/7751825/versions/2
+wget --output-document='2019-02-14-PPTC_FPKM_matrix_withModelID-244.rda' https://ndownloader.figshare.com/articles/7751825/versions/2
 
-# 5. focal CN matrix
-curl -o 'short_cn_matrix_fpkm1.txt' https://ndownloader.figshare.com/files/14545979
+# 4. focal CN matrix
+wget --output-document='short_cn_matrix_fpkm1.txt' https://ndownloader.figshare.com/files/14545979
 
-# 6. download gene lists
-curl -o 'leukemia-goi-list.txt' https://ndownloader.figshare.com/files/14541185
-curl -o 'osteosarcoma-goi-list.txt' https://ndownloader.figshare.com/files/14541191
-curl -o 'renal-goi-list.txt' https://ndownloader.figshare.com/files/14541197
-curl -o 'brain-goi-list.txt' https://ndownloader.figshare.com/files/14541182
-curl -o 'neuroblastoma-goi-list.txt' https://ndownloader.figshare.com/files/14541188
-curl -o 'rare-goi-list.txt' https://ndownloader.figshare.com/files/14541194
-curl -o 'sarcoma-goi-list.txt' https://ndownloader.figshare.com/files/14541200
+# 5. download gene lists
+wget --output-document='leukemia-goi-list.txt' https://ndownloader.figshare.com/files/14541185
+wget --output-document='osteosarcoma-goi-list.txt' https://ndownloader.figshare.com/files/14541191
+wget --output-document='renal-goi-list.txt' https://ndownloader.figshare.com/files/14541197
+wget --output-document='brain-goi-list.txt' https://ndownloader.figshare.com/files/14541182
+wget --output-document='neuroblastoma-goi-list.txt' https://ndownloader.figshare.com/files/14541188
+wget --output-document='rare-goi-list.txt' https://ndownloader.figshare.com/files/14541194
+wget --output-document='sarcoma-goi-list.txt' https://ndownloader.figshare.com/files/14541200
 
-# 7. Neuroblastoma specific leisons
-curl -o 'neuroblastoma-specific-lesions.txt' https://ndownloader.figshare.com/files/14545973
+# 6. Neuroblastoma specific leisons
+wget --output-document='neuroblastoma-specific-lesions.txt' https://ndownloader.figshare.com/files/14545973
 
-# 8. Classifier scores
-curl -o "classifier_scores_with_clinical_and_alterations.tsv" https://ndownloader.figshare.com/files/14545970
+# 7. Classifier scores
+wget --output-document="classifier_scores_with_clinical_and_alterations.tsv" https://ndownloader.figshare.com/files/14545970
 
-# 9. arm leisons
-curl -o 'arm-lesions.txt' https://ndownloader.figshare.com/files/14545967
+# 8. arm leisons
+wget --output-document='arm-lesions.txt' https://ndownloader.figshare.com/files/14545967
 
-# 10. Driverfusions collapsed
-curl -o 'DriverFusions_Collapsed.txt' https://ndownloader.figshare.com/files/14545976
+# 9. Driverfusions collapsed
+wget --output-document='DriverFusions_Collapsed.txt' https://ndownloader.figshare.com/files/14545976
 
-#11. renal specific leisons
-curl -o 'renal-specific-lesions.txt' https://ndownloader.figshare.com/files/14550614
+#10. renal specific leisons
+wget --output-document='renal-specific-lesions.txt' https://ndownloader.figshare.com/files/14550614
