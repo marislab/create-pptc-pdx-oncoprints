b2dd2c093a894198d4708bffba3b6ea41d079fd1
Khushbu Patel <kpatel427@gatech.edu>
2019-03-14 13:23:26 -0400

    update

--- load-maf-maftools.R
+++ load-maf-maftools.R
@@ -9,7 +9,7 @@ colnames(clin.maf)[colnames(clin.maf) == "Model"] <- "Tumor_Sample_Barcode"
 #clin.pptc <- subset(clin.maf, Model.Part.of.PPTC == "yes")
 
 
-maf = read.maf(maf = pptc.merge, clinicalData = clin, vc_nonSyn = c("Frame_Shift_Del", 
+maf = read.maf(maf = pptc.merge, clinicalData = clin.maf, vc_nonSyn = c("Frame_Shift_Del", 
                                                                          "Frame_Shift_Ins", "Splice_Site", "Translation_Start_Site","Nonsense_Mutation", 
                                                                          "Nonstop_Mutation", "In_Frame_Del","In_Frame_Ins", "Missense_Mutation",  
                                                                          "Stop_Codon_Ins", "Start_Codon_Del"))
