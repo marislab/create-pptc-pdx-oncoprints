39b0baecb10de8d08be0e74a1c46eabc4f96dce5
Jo Lynne Rokita <jolynnerokita@d3b.center>
2019-09-16 12:40:06 -0400

    swap order for dependent packages

--- R/install-packages.R
+++ R/install-packages.R
@@ -1,8 +1,8 @@
-devtools::install_github(repo = "marislab/maftools")
-
 if (!require("devtools")){
   install.packages("devtools", repos='http://cran.us.r-project.org', dependencies = TRUE)
 }
+devtools::install_github(repo = "marislab/maftools")
+
 if (!require("rmatio")){
   install.packages("rmatio", repos='http://cran.us.r-project.org', dependencies = TRUE)
 }
